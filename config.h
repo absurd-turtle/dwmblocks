42d586fdcbe7d300b63e6724230d75a055953909
absurd-turtle <absurd.turtl3@gmail.com>
2022-05-09 18:04:10 +0300

    changed icon and sb-volume path

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"󰍛", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	        30,		1},
-	{"", "$HOME/.config/bar-scripts/scripts/sb-volume-icoMoon", 					  	5,		2},
+	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	        30,		1},
+	{"", "$HOME/.config/bar-scripts/scripts/sb-volume",         					  	5,		2},
 	{"", "$HOME/.config/bar-scripts/scripts/sb-battery",	 		          			60,		3},
 	{"", "$HOME/.config/bar-scripts/scripts/sb-internet", 					        	60,   4},
 	{"", "$HOME/.config/bar-scripts/scripts/sb-disk", 						            600,	5},
@@ -10,5 +10,5 @@ static const Block blocks[] = {
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char *delim = "  ";
+static char *delim = " \\ ";
 static unsigned int delimLen = 5;
